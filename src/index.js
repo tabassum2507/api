71ba417ed32400b635882bc79a9170f8bcca9634
tabassum2507 <10tabassum2@gmail.com>
2021-10-05 23:41:17 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+const ads = [
+      {title: 'Hello, world (again)!'}
+    ];
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+      res.send(ads);
+    });
+
+app.listen(3001, () => {
+    console.log('listening on port 3001');
+    });
